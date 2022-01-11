0b71c2fd22bbde5685f9e05ddbe9b5c3bf2e5b4e
daipon333 <dqllc0n@gmail.com>
2022-01-11 19:56:31 +0900

    npm-start

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,16 +1,16 @@
-import proxy from "http-proxy-middleware";
-import getAccountApi from "./components/getAccountApi";
+// import proxy from "http-proxy-middleware";
+// import getAccountApi from "./components/getAccountApi";
 
-module.exports = function (app) {
-  const headers = {
-    "Content-Type": "application/json",
-  };
-  app.use(
-    proxy(getAccountApi, {
-      target: "https://test.com/",
-      changeOrigin: true,
-      secure: false,
-      headers: headers,
-    })
-  );
-};
+// module.exports = function (app) {
+//   const headers = {
+//     "Content-Type": "application/json",
+//   };
+//   app.use(
+//     proxy(getAccountApi, {
+//       target: "https://test.com/",
+//       changeOrigin: true,
+//       secure: false,
+//       headers: headers,
+//     })
+//   );
+// };
