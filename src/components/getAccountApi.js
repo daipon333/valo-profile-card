ee3610b3284a5bdaa5c64c7c86a24b716bf09b8b
daipon333 <dqllc0n@gmail.com>
2022-01-12 12:31:50 +0900

    setupProxy.jsの削除

--- src/components/getAccountApi.js
+++ src/components/getAccountApi.js
@@ -1,19 +1,6 @@
 import React from "react";
 import axios from "axios";
 
-// axios.defaults.baseURL = "http://localhost:3000";
-// axios.defaults.headers.post["Content-Type"] = "application/json;charset=utf-8";
-// axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
-// axios.defaults.headers.post["Access-Control-Allow-Methods"] = "*";
-
-// const options = {
-//   headers: {
-//     "Access-Control-Allow-Origin": "*",
-//     "Access-Control-Allow-Methods": "POST,GET,PUT,DELETE",
-//     "Access-Control-Allow-Credentials": "true",
-//   },
-// };
-
 function getAccountApi(id, tag) {
   const AccountApi = `https://asia.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${id}/${tag}?api_key=RGAPI-2cc040ae-77c5-482e-94de-8ffa6fd81a9c`;
   const accountInfo = async () => {
